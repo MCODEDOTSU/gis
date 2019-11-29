6ad3717bd4c2684c3e79f85ea065ed8e226a394a
leliaoff <leliaoff@ya.ru>
2019-11-29 08:25:57 +0400

    Signed-off-by: leliaoff <leliaoff@ya.ru>

---
+++ precache-manifest.68eeabaadea5e0f6fb4533893b80b2e9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "79bce5b9223882f1e45491aa90c3b2e1",
+    "url": "/sass/manager.scss"
+  },
+  {
+    "revision": "57148db760b7e9f7eb795a514bac747f",
+    "url": "/sass/images/background.jpg"
+  },
+  {
+    "revision": "ff8e4a10fe0ace5e442d8cceeff1f652",
+    "url": "/sass/images/logoBackground.png"
+  },
+  {
+    "revision": "5cfcf79a62beafdcdd236e641d365537",
+    "url": "/sass/images/city.png"
+  },
+  {
+    "revision": "f21adcfb1e808968d259c7b9bdb82253",
+    "url": "/sass/responsive.scss"
+  },
+  {
+    "revision": "8a62a3aadea983b26a1c9d562e0b8e99",
+    "url": "/images/mcode.png"
+  },
+  {
+    "revision": "b6067315421d4d2d97aacac547377f9a",
+    "url": "/sass/_variables.scss"
+  },
+  {
+    "revision": "cb030f811c5dbbbe8ea44c73f370416f",
+    "url": "/sass/app.scss"
+  },
+  {
+    "revision": "b678361998b1ac9870a08f585c17ed5e",
+    "url": "/sass/customer.scss"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0643484b84975342bd5935ee36560d96",
+    "url": "/js/ol.js"
+  },
+  {
+    "revision": "cd75f26ad7787f64f876",
+    "url": "/js/chunk-vendors.f954c632.js"
+  },
+  {
+    "revision": "21d6de39e6a9f5e65fce",
+    "url": "/js/app.3b8acb85.js"
+  },
+  {
+    "revision": "1fbf4c0b97ee6b86ec27af0528fa035a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed95e82e2ab22b48d267491abb27f552",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "9307a55d70f7a6c5db88a42baeee5fe1",
+    "url": "/css/ol.css"
+  },
+  {
+    "revision": "cd75f26ad7787f64f876",
+    "url": "/css/chunk-vendors.f35f7e1f.css"
+  },
+  {
+    "revision": "21d6de39e6a9f5e65fce",
+    "url": "/css/app.11d5a3f7.css"
+  },
+  {
+    "revision": "f1f1ed222efaef3169362d517ff33c57",
+    "url": "/.htaccess"
+  }
+];
---
+++ precache-manifest.b9d081c5dc12d17a694b7dc3676de582.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1f1ed222efaef3169362d517ff33c57",
+    "url": "/.htaccess"
+  },
+  {
+    "revision": "0643484b84975342bd5935ee36560d96",
+    "url": "/js/ol.js"
+  },
+  {
+    "revision": "57148db760b7e9f7eb795a514bac747f",
+    "url": "/sass/images/background.jpg"
+  },
+  {
+    "revision": "ff8e4a10fe0ace5e442d8cceeff1f652",
+    "url": "/sass/images/logoBackground.png"
+  },
+  {
+    "revision": "5cfcf79a62beafdcdd236e641d365537",
+    "url": "/sass/images/city.png"
+  },
+  {
+    "revision": "b6067315421d4d2d97aacac547377f9a",
+    "url": "/sass/_variables.scss"
+  },
+  {
+    "revision": "79bce5b9223882f1e45491aa90c3b2e1",
+    "url": "/sass/manager.scss"
+  },
+  {
+    "revision": "f21adcfb1e808968d259c7b9bdb82253",
+    "url": "/sass/responsive.scss"
+  },
+  {
+    "revision": "b678361998b1ac9870a08f585c17ed5e",
+    "url": "/sass/customer.scss"
+  },
+  {
+    "revision": "cb030f811c5dbbbe8ea44c73f370416f",
+    "url": "/sass/app.scss"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cd75f26ad7787f64f876",
+    "url": "/js/chunk-vendors.f954c632.js"
+  },
+  {
+    "revision": "a7870f2eb5e8ff538918",
+    "url": "/js/app.9e0a1344.js"
+  },
+  {
+    "revision": "f474fbc8b356b3948c3af6be240718d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a62a3aadea983b26a1c9d562e0b8e99",
+    "url": "/images/mcode.png"
+  },
+  {
+    "revision": "ed95e82e2ab22b48d267491abb27f552",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "9307a55d70f7a6c5db88a42baeee5fe1",
+    "url": "/css/ol.css"
+  },
+  {
+    "revision": "cd75f26ad7787f64f876",
+    "url": "/css/chunk-vendors.f35f7e1f.css"
+  },
+  {
+    "revision": "a7870f2eb5e8ff538918",
+    "url": "/css/app.11d5a3f7.css"
+  }
+];
